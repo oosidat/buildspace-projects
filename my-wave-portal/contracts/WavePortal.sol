50fd7fe30cfeec6b170bbe6986979845ababcf37
Osama Sidat <oosidat@gmail.com>
2021-10-24 15:02:17 -0400

    [waveportal] Contract with wave count and array to track wavers

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -5,7 +5,25 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaveCount;
+    address[] wavers;
+
     constructor() {
-        console.log("Yo yo yo, imma contract and i'm smart!");
+        console.log("WavePortalContractConstructor");
+    }
+
+    function wave() public {
+        totalWaveCount += 1;
+        wavers.push(msg.sender);
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaveCount() public view returns (uint256) {
+        console.log("totalWaveCount: %d", totalWaveCount);
+        return totalWaveCount;
+    }
+
+    function getWavers() public view returns (address[] memory) {
+        return wavers;
     }
 }
--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,8 +1,28 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-  console.log(`contract deployed to: ${waveContract.address}`);
+
+  console.log(`contract deployed_to: ${waveContract.address}`);
+  console.log(`contract deployed_by: ${owner.address}`);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaveCount();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaveCount();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaveCount();
+
+  let wavers = await waveContract.getWavers();
+  console.log("wavers: ", wavers);
 };
 
 const runMain = async () => {
