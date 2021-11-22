aa1aacf59db51fdc065f70306ef42d6a6360af9b
Osama Sidat <oosidat@gmail.com>
2021-11-22 17:14:06 -0500

    [epic-nfts] Minting an NFT on testnet

--- epic-nfts/contracts/MyEpicNFT.sol
+++ epic-nfts/contracts/MyEpicNFT.sol
@@ -2,10 +2,30 @@
 
 pragma solidity ^0.8.0;
 
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+// Console.log library
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
-        console.log("Dis my NFT contract");
+// inherit an OpenZepellin contract - prevents boiler plate
+contract MyEpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("this is my NFT contract");
+    }
+
+    function makeEnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/1O2M");
+        console.log("NFT id:%s minted_to:%s", newItemId, msg.sender);
+
+        _tokenIds.increment();
     }
 }
